4d247eab9744a337807051253d64b4858dc01b15
Sandro Padin <sandropadin@gmail.com>
2013-05-20 15:43:14 -0500

    adding game

---
+++ game.go
@@ -0,0 +1,31 @@
+package tictactoe
+
+type Game struct {
+	board *Board
+}
+
+func winningCombinations() [][3]int {
+	return [][3]int{
+		[3]int{0, 1, 2}, [3]int{4, 5, 6}, [3]int{7, 8, 9},
+		[3]int{0, 3, 6}, [3]int{1, 4, 7}, [3]int{2, 5, 8},
+		[3]int{0, 4, 8}, [3]int{2, 4, 6},
+	}
+}
+
+func (game *Game) IsGameover() bool {
+	return game.HasWinner() || game.board.IsFull()
+}
+
+func (game *Game) HasWinner() bool {
+	board := game.board
+	for _, combination := range winningCombinations() {
+		if board[combination[0]] == E {
+			continue
+		}
+		if board[combination[0]] == board[combination[1]] &&
+			board[combination[0]] == board[combination[2]] {
+			return true
+		}
+	}
+	return false
+}
---
+++ game_test.go
@@ -0,0 +1,53 @@
+package tictactoe
+
+import (
+	"github.com/remogatto/prettytest"
+	"testing"
+)
+
+type testGameSuite struct {
+	prettytest.Suite
+}
+
+func gameSuiteSetup() (board *Board, game *Game) {
+	board = new(Board)
+	game = new(Game)
+	game.board = board
+	return
+}
+
+func TestGameRunner(t *testing.T) {
+	prettytest.RunWithFormatter(
+		t,
+		new(prettytest.TDDFormatter),
+		new(testGameSuite),
+	)
+}
+
+func (t *testGameSuite) TestNewGameHasNoWinner() {
+	_, game := gameSuiteSetup()
+	t.False(game.HasWinner(), "New game has no winner")
+}
+
+func (t *testGameSuite) TestXWinningGameHasWinner() {
+	board, game := gameSuiteSetup()
+	playXWinningGame(board)
+	t.True(game.HasWinner(), "X winning game should have winner")
+}
+
+func (t *testGameSuite) TestGameoverFalseWithNewGame() {
+	_, game := gameSuiteSetup()
+	t.False(game.IsGameover(), "New game is not gameover")
+}
+
+func (t *testGameSuite) TestGameoverTrueWithWinningGame() {
+	board, game := gameSuiteSetup()
+	playXWinningGame(board)
+	t.True(game.IsGameover(), "Game is over when there is a winner")
+}
+
+func (t *testGameSuite) TestGameoverTrueWithFullBoard() {
+	board, game := gameSuiteSetup()
+	playTiedGame(board)
+	t.True(game.IsGameover(), "Game is over when board full")
+}
