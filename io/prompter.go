a8c072ae472fd2ed67fa57b80e58f1285c60e2b2
Sandro Padin <sandropadin@gmail.com>
2013-05-21 17:06:14 -0500

    adding choice prompting

--- io/prompter.go
+++ io/prompter.go
@@ -1,5 +1,10 @@
 package io
 
+import (
+	"strconv"
+	"strings"
+)
+
 type Prompter struct {
 	in  *Getter
 	out *Printer
@@ -11,5 +16,29 @@ func (p *Prompter) PromptInt(message string) (input int) {
 		p.out.Println("\nInvalid input, please enter a number")
 		p.out.Print(message)
 	})
+	return
+}
+
+func (p *Prompter) PromptChoice(message string, choices ...string) (selection string) {
+	p.out.Println(message)
+	for index, choice := range choices {
+		choiceStr := strings.Join([]string{strconv.Itoa(index + 1), ". ", choice}, "")
+		p.out.Println(choiceStr)
+	}
+
+	selectionInt := p.PromptInt("Enter your choice:")
+	selectionIdx := selectionInt - 1
+	for selectionIdx > len(choices) || selectionIdx < 0 {
+		p.out.Println("\nInvalid input, please enter a number from the choice list")
+		selectionInt = p.PromptInt("Enter your choice:")
+		selectionIdx = selectionInt - 1
+
+	}
+	selection = choices[selectionIdx]
+	return
+}
+
+func NewPrompter() (prompter *Prompter) {
+	prompter = &Prompter{in: NewStdinGetter(), out: NewStdoutPrinter()}
 	return
 }
--- io/prompter_test.go
+++ io/prompter_test.go
@@ -3,6 +3,7 @@ package io
 import (
 	"github.com/remogatto/prettytest"
 	"github.com/spadin/tictactoe/io/helpers"
+	"os"
 	"testing"
 )
 
@@ -47,4 +48,37 @@ func (t *testPrompterSuite) TestPromptIntRepeatedly() {
 
 	t.Equal("Enter a number:\nInvalid input, please enter a number\nEnter a number:", output)
 	t.Equal(1, input)
+}
+
+func (t *testPrompterSuite) TestNewPrompter() {
+	prompter := NewPrompter()
+	t.Equal(os.Stdin, prompter.in.in, "sets Stdin to getter")
+	t.Equal(os.Stdout, prompter.out.out, "sets Stdout to printer")
+
+}
+
+func (t *testPrompterSuite) TestPromptChoice() {
+	var choice string
+	output := iohelpers.CaptureOutput(func() {
+		iohelpers.SimulateInput("1", func() {
+			prompter := prompterSuiteSetup()
+			choice = prompter.PromptChoice("Choose from the following", "banana", "apple")
+		})
+	})
+
+	t.Equal("Choose from the following\n1. banana\n2. apple\nEnter your choice:", output)
+	t.Equal("banana", choice, "should choose choice 1 and return choice string")
+}
+
+func (t *testPrompterSuite) TestPromptChoiceWithInvalidInputs() {
+	var choice string
+	output := iohelpers.CaptureOutput(func() {
+		iohelpers.SimulateMultipleInput([]string{"5", "1"}, func() {
+			prompter := prompterSuiteSetup()
+			choice = prompter.PromptChoice("Choose from the following", "banana", "apple")
+		})
+	})
+
+	t.Equal("Choose from the following\n1. banana\n2. apple\nEnter your choice:\nInvalid input, please enter a number from the choice list\nEnter your choice:", output)
+	t.Equal("banana", choice, "should choose choice 1 and return choice string")
 }
