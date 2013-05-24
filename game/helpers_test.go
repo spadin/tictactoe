a586cb53014b060811cc7927a2fc24769952a0b6
Sandro Padin <sandropadin@gmail.com>
2013-05-24 10:01:53 -0500

    adding test coverage for game helpers

---
+++ game/helpers_test.go
@@ -0,0 +1,47 @@
+package game
+
+import (
+	"github.com/remogatto/prettytest"
+	"testing"
+)
+
+type testGameHelpersSuite struct {
+	prettytest.Suite
+}
+
+func TestGameHelpersRunner(t *testing.T) {
+	prettytest.RunWithFormatter(
+		t,
+		new(prettytest.TDDFormatter),
+		new(testGameHelpersSuite),
+	)
+}
+
+func (t *testGameHelpersSuite) TestIsEven() {
+	t.True(isEven(0))
+	t.False(isEven(1))
+	t.True(isEven(2))
+}
+
+func (t *testGameHelpersSuite) TestMakeMoves() {
+	board := NewTictactoeBoard()
+	makeMoves(board, 0, 1)
+	t.Equal(X, board[0])
+	t.Equal(O, board[1])
+}
+
+func (t *testGameHelpersSuite) TestPlayXWinningGame() {
+	board := NewTictactoeBoard()
+	game := NewTictactoeGame(board)
+	playXWinningGame(board)
+	t.True(game.IsGameover())
+	t.Equal(X, game.Winner())
+}
+
+func (t *testGameHelpersSuite) TestPlayTiedGame() {
+	board := NewTictactoeBoard()
+	game := NewTictactoeGame(board)
+	playTiedGame(board)
+	t.True(game.IsGameover())
+	t.Nil(game.Winner())
+}
