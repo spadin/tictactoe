cdfc345dba8e8793741a00821e5d3619fcbbe344
Sandro Padin <sandropadin@gmail.com>
2013-05-24 20:47:30 -0500

    fixing winning combination

--- game/tictactoe_rules.go
+++ game/tictactoe_rules.go
@@ -11,7 +11,7 @@ func NewTictactoeRules(board Board) (rules Rules) {
 
 func winningCombinations() [][3]int {
 	return [][3]int{
-		[3]int{0, 1, 2}, [3]int{4, 5, 6}, [3]int{7, 8, 9},
+		[3]int{0, 1, 2}, [3]int{3, 4, 5}, [3]int{6, 7, 8},
 		[3]int{0, 3, 6}, [3]int{1, 4, 7}, [3]int{2, 5, 8},
 		[3]int{0, 4, 8}, [3]int{2, 4, 6},
 	}
